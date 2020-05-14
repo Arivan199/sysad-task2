8ed1299a253c7a496abdca80bb1ad3f9d1a95935
Arivan199 <63254290+Arivan199@users.noreply.github.com>
2020-05-14 12:31:40 +0530

    Delete data.sql

--- data.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE Complaint_Portal;
-CREATE TABLE Complaint_Portal.complaint_table(
-	id INT(10) NOT NULL AUTO_INCREMENT,
-	username VARCHAR(20) NOT NULL,
-	complaint VARCHAR(50) NOT NULL,
-	time TIMESTAMP NOT NULL DEFAULT NOW(),
-	PRIMARY KEY (id)
-	);
