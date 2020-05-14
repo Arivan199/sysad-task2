6f62236bfbdf8c7feed951100e42d92ea706842d
Arivan199 <63254290+Arivan199@users.noreply.github.com>
2020-05-14 12:08:47 +0530

    automated script to run container

---
+++ setup2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#build the image
+sudo docker build -t ubuntu:2.0 .
+
+#run the container
+x = echo "docker images|grep ubuntu*2.0|awk 'print{$1}'"
+docker -itd run $x /bin/bash
+
+#run the alias.sh inside the container
+y = echo "docker ps|grep $x|awk 'print{$1}'"
+docker exec -it $y sh aliases.sh
