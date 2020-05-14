f841b0b03510612cda09c09c4f1ebd68e7f9743c
Arivan199 <63254290+Arivan199@users.noreply.github.com>
2020-05-14 17:18:41 +0530

    hackermode-2

--- hackermode/insert.php
+++ hackermode/insert.php
@@ -1,5 +1,5 @@
 <?php
-        $con =mysqli_connect('database','newuser','newword');
+        $con =mysqli_connect('localhost','root','root');
         if(!$con)
         {
                 echo "Not connected to server";
