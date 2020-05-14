a376f3145d7683e5964f7bcce1ebad14c7972a56
Arivan199 <63254290+Arivan199@users.noreply.github.com>
2020-05-14 22:31:38 +0530

    Update proxy.sh

--- hackermode/proxy.sh
+++ hackermode/proxy.sh
@@ -1,7 +1,7 @@
 #port 80 of the container is mapped outside with port 3333 .therfore reverse proxy soldier.io to 127.0.0.1:3333 opens complaint page inside the container 
 sudo echo "<VirtualHost *:80>
    ProxyPass / http://127.0.0.1:3333/
-      </VirtualHost>                  " > /etc/apache2/sites-available/soldier.io.conf
+      </VirtualHost>                  " >> /etc/apache2/sites-available/soldier.io.conf
 
 #need to enable proxy and proxy_http mods 
 sudo a2ensite soldier.io
